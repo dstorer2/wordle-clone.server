a8694a74cfc1349e07a5cda063d7f1cce99e3b97
David Storer <dstorer2@gmail.com>
2022-02-11 22:06:30 -0500

    port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 1234;
+const port = process.env.PORT || 1234;
 const cors = require("cors");
 const axios = require("axios");
 require('dotenv').config();
